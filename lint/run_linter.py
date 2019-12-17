13df2624fdb6e018717a104c1a66f631f7bfd870
Rodrigo Fagundes <rmfagundes@gmail.com>
2019-12-17 14:53:58 -0300

    Traversing repo and generating report.

--- lint/run_linter.py
+++ lint/run_linter.py
@@ -8,6 +8,10 @@ conf = YamlLintConfig('extends: default')
 rep = open('./lint/report.txt', "a+")
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
-        if '.yml' in file:
-            f = open(os.path.join(subdir, file))
-            rep.write(linter.run(f, conf))
+        if '.yaml' in file:
+            rel_path = os.path.join(subdir, file)
+            f = open(rel_path)
+            gen = linter.run(f, conf)
+            for lint in list(gen):
+                rep.write(f'{rel_path}:{lint.line}:{lint.column}: [warning] {lint.desc} ({lint.rule})\n')
+rep.close()
